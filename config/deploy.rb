5a7e8112de80d476e73c4bec5e8654e2dc834f40
Preston Lee <conmotto@gmail.com>
2013-11-08 16:35:05 -0700

    Upgrading to Capistrano v3.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 set :application, 'stakeout'
-set :repo_url, 'git@example.com:me/stakeout.git'
+set :repo_url, 'git@github.com:preston/stakeout.git'
 
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
 
